1a53793b139e383add06c67b2329f17689eca175
Yan <yan.matagne@gmail.com>
2024-10-02 14:54:52 -0400

    add dependencies

--- wellcode-cli.rb
+++ wellcode-cli.rb
@@ -70,6 +70,11 @@ class WellcodeCli < Formula
       url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
       sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
     end
+
+    resource "certifi" do
+      url "https://files.pythonhosted.org/packages/98/98/c2ff18671db109c9f10ed27f5ef610ae05b73bd876664139cf95bd1429aa/certifi-2023.7.22.tar.gz"
+      sha256 "539cc1d13202e33ca466e88b2807e29f4c13049d6d87031a3c110744495cb082"
+    end
     
     def install
       virtualenv_install_with_resources
