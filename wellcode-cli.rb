1a3be17a5688aedf8f6ec1ef2826e93041aed404
Yan <yan.matagne@gmail.com>
2024-10-02 14:04:58 -0400

    first commit

--- wellcode-cli.rb
+++ wellcode-cli.rb
@@ -10,6 +10,9 @@ class WellcodeCli < Formula
     #depends_on "python@3.11"
     #depends_on "numpy"
     #depends_on "openblas"
+    depends_on "python@3.11"
+    depends_on "virtualenv"
+
     
     resource "PyGithub" do
       url "https://files.pythonhosted.org/packages/98/36/386d282903c572b18abc36de68aaf4146db4659c82dceee009ef88a86b67/PyGithub-1.55.tar.gz"
