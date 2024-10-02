9541843ccdf0a3795f9693f91bda61ed7fe85ac4
ymatagne <yan.matagne@gmail.com>
2024-10-02 16:37:50 -0400

    v0.1.6 release

--- wellcode-cli.rb
+++ wellcode-cli.rb
@@ -3,8 +3,8 @@ class WellcodeCli < Formula
 
   desc "Engineering Team Metrics Script"
   homepage "https://github.com/wellcode-ai/wellcode-cli"
-  url "https://github.com/wellcode-ai/wellcode-cli/archive/v0.1.5.tar.gz"
-  sha256 "772048818e29c79a51f6fa7ef631650ad62a78564e1bc420b6a62566003b4927"
+  url "https://github.com/wellcode-ai/wellcode-cli/archive/v0.1.6.tar.gz"
+  sha256 "0577aa56ec7df6f40599b5dfa6b5687f9a0404105d46129a77d08166ad8ed7ee"
   license "MIT"
 
   depends_on "python@3.11"
@@ -61,6 +61,6 @@ class WellcodeCli < Formula
     chmod 0755, bin/"wellcode-cli"
   end
   test do
-    assert_match "Wellcode CLI version 0.1.5", shell_output("#{bin}/wellcode-cli --version")
+    assert_match "Wellcode CLI version 0.1.6", shell_output("#{bin}/wellcode-cli --version")
   end
 end
